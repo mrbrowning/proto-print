7c0bfa9d6cc901d31e24c0e946032915c10f6492
Michael Browning <mbrowning@mesosphere.com>
2018-02-09 16:44:14 -0800

    asdf

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveFunctor #-}
+
 module Main where
 
 import Control.Monad.State
@@ -7,6 +9,7 @@ import System.Environment (getArgs)
 import Text.Parsec hiding (State)
 import Text.Parsec.String (Parser, parseFromFile)
 
+newtype Fix f = Fix { unFix :: f (Fix f) }
 
 type Identifier = String
 type Value = String
@@ -14,6 +17,9 @@ data Object = Object Identifier [Item] deriving Show
 data Pair = Pair Identifier Value deriving Show
 data Item = ObjectItem Object | PairItem Pair deriving Show
 
+data Lit = Ident String | Val String deriving Show
+data ProtoExprF a = Obj Lit [a] | P Lit Lit deriving (Show, Functor)
+type ProtoExpr = Fix ProtoExprF
 
 concat3 :: [a] -> [a] -> [a] -> [a]
 concat3 xs ys zs = xs ++ ys ++ zs
@@ -22,13 +28,22 @@ concat3 xs ys zs = xs ++ ys ++ zs
 identifier :: Parser Identifier
 identifier = many1 $ lower <|> char '_'
 
+identifierF :: Parser Lit
+identifierF = Ident <$> (many1 $ lower <|> char '_')
+
 -- <value> ::= <numberValue>|<stringValue>|<enumValue>
 value :: Parser Value
 value = stringValue <|> numberValue <|> enumValue <|> booleanValue
 
+valueF :: Parser Lit
+valueF = Val <$> (stringValue <|> numberValue <|> enumValue <|> booleanValue)
+
 -- <pair> ::= <identifier>: <value>
 pair :: Parser Pair
 pair = spaces *> (Pair <$> (identifier <* char ':' <* space) <*> value) <* spaces
+
+pairF :: Parser ProtoExpr
+pairF = spaces *> (Fix <$> (P <$> (identifierF <* char ':' <* space) <*> valueF)) <* spaces
 
 -- <item> ::= <object>|<pair>
 item :: Parser Item
@@ -40,6 +55,14 @@ object :: Parser Object
 object =
     spaces *>
     (Object <$> (identifier <* spaces <* char '{') <* spaces <*> many item) <*
+    spaces <* char '}' <* spaces
+
+objectF :: Parser ProtoExpr
+objectF =
+    spaces *>
+    (Fix <$> (Obj <$>
+        (identifierF <* spaces <* char '{' <* spaces) <*>
+        (many (try pairF <|> objectF)))) <*
     spaces <* char '}' <* spaces
 
 -- <numberValue> ~ [0-9]+(\.[0-9]+(E[0-9]+)?)?
@@ -65,6 +88,26 @@ enumValue = many1 $ upper <|> char '_'
 -- <booleanValue> ::= true|false
 booleanValue :: Parser String
 booleanValue = string "true" <|> string "false"
+
+cata :: Functor f => (f a -> a) -> Fix f -> a
+cata f = f . fmap (cata f) . unFix
+
+zygo :: Functor f => (f b -> b) -> (f (b, a) -> a) -> Fix f -> a
+zygo f g = snd . cata (\x -> (f $ fmap fst x, g x))
+
+chi :: ProtoExprF String -> String
+chi (P _ _) = ""
+chi (Obj _ (x:_)) = "x  x" ++ x
+
+phi :: ProtoExprF (String, [String]) -> [String]
+phi (P (Ident k) (Val v)) = [k ++ ": " ++ v]
+phi (Obj (Ident k) ((indent, x):xs)) =
+    [indent ++ k ++ " {"] ++
+    (map (\s -> indent ++ "    " ++ s) x) ++
+    (concat $ map (\(indent, ys) -> map (\s -> indent ++ s) ys) xs) ++
+    ["}"]
+
+doit = zygo chi phi
 
 printItems :: [Item] -> State String String
 printItems items = do
